9122673be1df028e8d8a1cd281d38d1cb550b0da
Renaud BERNARD <renaud.bernard@p59production.com>
2018-03-21 14:11:14 +0100

    Preliner buil
    I build a preliner for center the form but has some element bogues.

--- ExercicesPOO/src/main/java/ch/conceptforge/exercicespoo/Exercice03/Sapin.java
+++ ExercicesPOO/src/main/java/ch/conceptforge/exercicespoo/Exercice03/Sapin.java
@@ -12,14 +12,44 @@ package ch.conceptforge.exercicespoo.Exercice03;
 public class Sapin {
     private int hauteur;
     String ligne = "*";
-    
+    String preLine = "";
+    String preFixe = "          ";
     public Sapin(int hauteur) {
         this.hauteur = hauteur;
-        System.out.println(ligne);
-        for (int i = 1; i <= hauteur ; i++) {
-            ligne = ligne + " *";
-            System.out.println(ligne);
+       
+        System.out.println("Sapin Coller");
+               for (int i = 1; i <= (hauteur /2) ; i++) {
+            preLine = preLine + " ";
+        }//END for
+        System.out.println(preFixe + preLine + ligne);
+        for (int i = 1; i < hauteur ; i++) {
+            preLine = "";
+            for (int j = 1; j <= ((hauteur-(i)) /2) ; j++) {
+            preLine = preLine + " ";
+            }//END for
+            ligne = ligne + "**";
+            
+            System.out.println(preFixe +preLine + ligne);
         }//END FOR
+        
+        
+        
+        System.out.println("Sapin ouvert");
+        //Preliner
+
+//        for (int i = 1; i <= (hauteur /2) ; i++) {
+//            preLine = preLine + " ";
+//        }//END for
+//        System.out.println(preFixe + preLine + ligne);
+//        for (int i = 1; i < hauteur ; i++) {
+//            preLine = "";
+//            for (int j = 1; j <= ((hauteur-(i*2)) /2) ; j++) {
+//            preLine = preLine + " ";
+//            }//END for
+//                       
+//            ligne = ligne + " *";
+//            System.out.println(preFixe +preLine + ligne);
+//        }//END FOR
                 
     }//END Construct
 
