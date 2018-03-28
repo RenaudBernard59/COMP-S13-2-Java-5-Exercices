c8d2b2302655aa9e61956a1de00df6a8203f18e7
Renaud BERNARD <renaud.bernard@p59production.com>
2018-03-28 15:42:50 +0200

    Make a 2nd version of Exo3
    Make a seconde version of the Exercice 3 with center tree.

---
+++ ExercicesPOO/src/main/java/ch/conceptforge/exercicespoo/Exercice03/SapinV2.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.conceptforge.exercicespoo.Exercice03;
+
+/**
+ *
+ * @author renob
+ */
+public class SapinV2 {
+    private int hauteur;
+    String ligne = "*";
+    String preLine = "";
+    String preFixe = "          ";
+    int ligneLength;
+    public SapinV2(int hauteur) {
+        this.hauteur = hauteur;
+        ////////////////////////////////////////////////////////
+        System.out.println("Sapin Coller");
+        System.out.println(preFixe + preLine + ligne);
+        for (int i = 1; i < hauteur ; i++) {
+            ligne = ligne + "**";
+            ligneLength = ligne.length()/2;
+            System.out.println(preFixe +preLine + ligne);
+        }//END FOR
+        ////////////////////////////////////////////////////////
+        System.out.println("Sapin ouvert");
+        ligne = "*";
+        System.out.println(preFixe + preLine + ligne);
+        for (int i = 1; i < hauteur ; i++) {
+            preLine = "";
+            
+            ligne = ligne + " *";
+            System.out.println(preFixe +preLine + ligne);
+        }//END FOR
+        ////////////////////////////////////////////////////////
+        System.out.println("Sapin horizontal");
+        ligne = "";
+        for (int i = 1; i <= hauteur ; i++) {
+            preLine = "";
+            
+            ligne = ligne + "*";
+            System.out.println(preFixe +preLine + ligne);
+        }//END FOR
+        
+        for (int j = hauteur; j > 0 ; j--) {
+            preLine = "";
+            ligne = "";
+                for (int i = 1; i < j ; i++) {
+                ligne = ligne + "*";
+                }//END FOR
+            System.out.println(preFixe +preLine + ligne);
+        }//END FOR
+        
+        
+        
+        
+    }//END Construct
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
