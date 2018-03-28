c0ef71b89a3e30c2f10d1f59356017e307848184
Renaud BERNARD <renaud.bernard@p59production.com>
2018-03-28 16:04:20 +0200

    Try V2
    Try V2 of Exercice 3 but don't work correctely.

--- ExercicesPOO/src/main/java/ch/conceptforge/exercicespoo/Exercice03/SapinV2.java
+++ ExercicesPOO/src/main/java/ch/conceptforge/exercicespoo/Exercice03/SapinV2.java
@@ -15,48 +15,55 @@ public class SapinV2 {
     String preLine = "";
     String preFixe = "          ";
     int ligneLength;
+    int bt = (hauteur*2)-1;
+    int i;
+    int bn = (i*2)-1;
+    
+    
+         
     public SapinV2(int hauteur) {
         this.hauteur = hauteur;
         ////////////////////////////////////////////////////////
         System.out.println("Sapin Coller");
         System.out.println(preFixe + preLine + ligne);
-        for (int i = 1; i < hauteur ; i++) {
+        for (int i = 1; i <= hauteur ; i++) {
+            
+            for (int j = 1 ; j<=(bt-bn)/2 ; j++) {
+                preLine = preLine + " ";
+
+            }
             ligne = ligne + "**";
-            ligneLength = ligne.length()/2;
+                    
             System.out.println(preFixe +preLine + ligne);
         }//END FOR
         ////////////////////////////////////////////////////////
         System.out.println("Sapin ouvert");
-        ligne = "*";
-        System.out.println(preFixe + preLine + ligne);
-        for (int i = 1; i < hauteur ; i++) {
-            preLine = "";
-            
-            ligne = ligne + " *";
-            System.out.println(preFixe +preLine + ligne);
-        }//END FOR
-        ////////////////////////////////////////////////////////
-        System.out.println("Sapin horizontal");
-        ligne = "";
-        for (int i = 1; i <= hauteur ; i++) {
-            preLine = "";
-            
-            ligne = ligne + "*";
-            System.out.println(preFixe +preLine + ligne);
-        }//END FOR
-        
-        for (int j = hauteur; j > 0 ; j--) {
-            preLine = "";
-            ligne = "";
-                for (int i = 1; i < j ; i++) {
-                ligne = ligne + "*";
-                }//END FOR
-            System.out.println(preFixe +preLine + ligne);
-        }//END FOR
-        
-        
-        
-        
+//        ligne = "*";
+//        System.out.println(preFixe + preLine + ligne);
+//        for (int i = 1; i < hauteur ; i++) {
+//            preLine = "";
+//            
+//            ligne = ligne + " *";
+//            System.out.println(preFixe +preLine + ligne);
+//        }//END FOR
+//        ////////////////////////////////////////////////////////
+//        System.out.println("Sapin horizontal");
+//        ligne = "";
+//        for (int i = 1; i <= hauteur ; i++) {
+//            preLine = "";
+//            
+//            ligne = ligne + "*";
+//            System.out.println(preFixe +preLine + ligne);
+//        }//END FOR
+//        
+//        for (int j = hauteur; j > 0 ; j--) {
+//            preLine = "";
+//            ligne = "";
+//                for (int i = 1; i < j ; i++) {
+//                ligne = ligne + "*";
+//                }//END FOR
+////            System.out.println(preFixe +preLine + ligne);
+//        }//END FOR
     }//END Construct
 
     public int getHauteur() {
